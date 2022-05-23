89e3d0b163bde7de010526ae0a5408119f95187d
sikit <738904954@qq.com>
2022-05-23 23:29:26 +0800

    配置信息改动

--- site.config.ts
+++ site.config.ts
@@ -2,23 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '87f842f04bf1442d9534e65e20ed97f7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '🐷🐻',
+  domain: 'jieyue.ltd',
+  author: '🐷🐻',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
